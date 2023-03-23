b09e4f688978d32237e1a81fade34a2d9ec55151
Shavqqat001 <nazriev.shavqatjon@gmail.com>
2023-03-23 14:03:42 +0500

    some changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 let interestForm = document.querySelector('.interest__form');
 let interestButton = document.querySelector('.interest__button');
+let interestSpan = interestForm.querySelector('.interest__form--arrival > input');
 
-interestButton.onclick = function () { 
+interestButton.onclick = () => {
     interestForm.classList.toggle('visually-hidden');
+    interestSpan.focus();
 }
+interestForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+});
